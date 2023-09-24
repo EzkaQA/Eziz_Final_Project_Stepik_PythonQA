3dd18483786b904d21816c1d307c73d930ea6416
Eziz Annamyradov <eziz.annamyradow@mail.ru>
2023-09-24 22:21:09 +0300

    parametrization added

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,22 @@
 from .pages.product_page import ProductPage
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"#open the page
-    page = ProductPage(browser, link)#check the basket total, save the price in variable basketTotal
-    page.open()#check the books price and save to the variable bookPrice
-    page.should_add_product_to_card()#check the books name and save it to the variable bookName
-    page.product_name_matches_added_product()#assert that book is added to the basket(names are matching)
-    page.product_price_matches_added_product()#assert that basket price changed according to added book price
+import pytest
 
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param(
+                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                      marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)  # check the basket total, save the price in variable basketTotal
+    page.open()  # check the books price and save to the variable bookPrice
+    page.should_add_product_to_card()  # check the books name and save it to the variable bookName
+    page.product_name_matches_added_product()  # assert that book is added to the basket(names are matching)
+    page.product_price_matches_added_product()  # assert that basket price changed according to added book price
